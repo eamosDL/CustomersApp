37b8eb22b14bd7cc3e19e2bfca67a7f26a2eb501
Eithne Amos <eamos@duqlight.com>
2019-05-28 11:08:31 -0400

    added customers and customer list components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title: string;
   
   constructor() {}
 
   ngOnInit() {
     // here we would call a service that gets us the data
-    this.title = 'My First Angular App'
   }
 }
